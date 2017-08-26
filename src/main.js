3285b40f6e6aa3496f9c14480662d44ff3bbd48e
mduraes1994 <mduraes1994@gmail.com>
2017-08-26 14:18:36 -0300

    Refact to use reduce

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 module.exports = {
-  sum(num1, num2) {
-    return num1 + num2;
+  sum(...nums) {
+    return nums.reduce((previous, current) => previous + current, 0);
   },
 
-  sub(num1, num2) {
-    return num1 - num2;
+  sub(...nums) {
+    return nums.reduce((previous, current) => previous - current);
   },
 
   div(num1, num2) {
     return num2 === 0 ? 'cannot divide by zero' : num1 / num2;
   },
 
-  mult(num1, num2) {
-    return num1 * num2;
+  mult(...nums) {
+    return nums.reduce((previous, current) => previous * current);
   },
 
 };
