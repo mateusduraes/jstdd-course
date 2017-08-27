6b388392a2d53f5db69e2c744d1875c2afd770c3
mduraes1994 <mduraes1994@gmail.com>
2017-08-27 15:55:39 -0300

    Fix lint warnings

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,7 @@
 const FizzBuzz = (num) => {
-  if(num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-  if(num % 3 === 0) return 'Fizz';
-  if(num % 5 === 0) return 'Buzz';
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
 
   return num;
 };
